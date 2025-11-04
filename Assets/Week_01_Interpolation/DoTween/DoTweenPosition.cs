7fd724d0dd4f0dff979550ed69f3e83950e49457
runbudget <162553866+runbudget@users.noreply.github.com>
2025-11-04 11:01:27 +0800

    Week1_Interpolation_DOTween
    last section of interpolation

--- Assets/Week_01_Interpolation/DoTween/DoTweenPosition.cs
+++ Assets/Week_01_Interpolation/DoTween/DoTweenPosition.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 
 public class DoTweenPosition : MonoBehaviour
 {
+    public Transform startPoint;
+    public Transform endPoint;
+    public float duration = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //tween the obj position from start to end point
+        //over the duration
+        //transform.DOMove(endPoint.position, duration);
+        ///transform.DOMove(endPoint.position, duration).SetEase(Ease.InOutQuad);
+        //tween the object pos and loop with a ping-pong effect
+        transform.DOMove(endPoint.position,
+                duration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
     }
 
     // Update is called once per frame
